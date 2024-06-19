aca9dc413d7bf1adfa4506e73304dc9309762f52
Meiado <gabrielmeiado@hotmail.com>
2024-06-19 15:51:03 -0300

    desafio iphone

---
+++ desafio-iphone/src/iPhone.java
@@ -0,0 +1,38 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    public void ligar(String numero) {
+        System.out.println("Ligando para o número: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba no navegador...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+}
